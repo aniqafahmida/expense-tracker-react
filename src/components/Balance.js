7544a1110d123b5e54aff22b706a3b0c50b1f962
Anika <anikafahmidahossain29@gmail.com>
2022-04-22 06:42:26 +0600

    button added on balance js

---
+++ src/components/Balance.js
@@ -0,0 +1,27 @@
+// import React from 'react'
+
+// export default Balance = () => {
+//   return (
+//     <>
+//         <h4>
+//             Your Balance
+//         </h4>
+//        <h1 id="Balance">$0.00</h1>
+//     </>
+//   )
+// }
+
+
+ import React from 'react'
+ 
+ export default function Balance() {
+   return (
+     <>
+       <h4>Your Balance</h4>
+       <h1 id="balance">$0.00</h1>
+       <button>send</button>
+     </>
+   )
+ }
+ 
+
